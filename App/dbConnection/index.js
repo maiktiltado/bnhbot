67c66baba04ca6a09ca2dd6daeaa3a8740471c15
30111971 <44382838+30111971@users.noreply.github.com>
2020-07-25 02:28:42 -0300

    MONGOURI

--- App/dbConnection/index.js
+++ App/dbConnection/index.js
@@ -1,12 +1,3 @@
 const mongoose = require('mongoose');
-
-let url;
-if(process.env.NODE_ENV === "production") {
-    url = "mongodb+srv://admin:admin@discord-bot.rkm7y.mongodb.net/bot?retryWrites=true&w=majority"
-} else {
-    url = 'mongodb://localhost/test'
-}
-
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
-
+mongoose.connect((process.env.MONGOLAB_URI || 'mongodb://localhost/test'), {useNewUrlParser: true, useUnifiedTopology: true});
 module.exports = mongoose
