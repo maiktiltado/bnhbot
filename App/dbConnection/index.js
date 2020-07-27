d4c79f5ca2a3a6c53d4cd2072c45acaeb3c59629
30111971 <44382838+30111971@users.noreply.github.com>
2020-07-27 00:50:31 -0300

    Finalmente

--- App/dbConnection/index.js
+++ App/dbConnection/index.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
-mongoose.connect((process.env.MONGOLAB_URI || 'mongodb://localhost/test'), {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect((process.env.MONGODB_URI || 'mongodb://localhost/test'), {useNewUrlParser: true, useUnifiedTopology: true});
 module.exports = mongoose
